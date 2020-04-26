c6f466acd8ed9959dd23b4219c71882193cd46e2
Rmarcial <rogerpires@gmail.com>
2020-04-26 23:05:52 +0100

    improvements in readability

--- palindrome.py
+++ palindrome.py
@@ -20,27 +20,14 @@ def is_palindrome_recur(sentence):
     if len(sentence) <= 1:
         return True
     else:
-        return first_char(sentence) == last_char(sentence) \
-               and is_palindrome_recur(middle(sentence))
-
-
-# Utils
-def first_char(sentence):
-    return sentence[0]
-
-
-def last_char(sentence):
-    return sentence[len(sentence)-1]
-
-
-def middle(sentence):
-    return sentence[1:-1]
+        return sentence[0] == sentence[-1] \
+               and is_palindrome_recur(sentence[1:-1])
 
 
 # Tests
-stc = ""
-for n in range(1000):
-    stc += "a"
+stc = "aaab baaa"
+# for n in range(1000):
+#    stc += "a"
 
 print(is_palindrome_iter(stc))
 print(is_palindrome_recur(stc))
