d994ab820954b35d94e0a9a1b703da6bde50da1e
Rmarcial <rogerpires@gmail.com>
2020-04-27 04:15:20 +0100

    refactoring of the recursive with memorization function

--- rabbits.py
+++ rabbits.py
@@ -27,31 +27,25 @@ def rabbits_recur(month):
     if month == 0 or month == 1:
         return 2
     else:
-        return rabbits_recur(month-2) + rabbits_recur((month-1))
+        return rabbits_recur(month - 2) + rabbits_recur((month - 1))
 
 
 # Recursive with dictionary - prevent several computations of the same value
 def rabbits_recur_dict(month):
-    computations_dict = {}
+    computed_dict = {0: 2, 1: 2}
 
     def compute(mth, dic):
-        if mth == 0 or mth == 1:
-            return 2
+        if mth in dic:
+            return dic[mth]
         else:
-            if mth-2 in dic and mth-1 in dic:
-                return dic[mth - 2] + dic[mth - 1]
-            else:
-                dic[mth - 2] = compute(mth - 2, dic)
-                dic[mth - 1] = compute(mth - 1, dic)
-                return dic[mth - 2] + dic[mth - 1]
+            dic[mth] = compute(mth - 2, dic) + compute(mth - 1, dic)
+            return dic[mth]
 
-    return compute(month, computations_dict)
+    return compute(month, computed_dict)
 
 
 # Tests
-nr = 37
+nr = 32
 print(rabbits_iter(nr))
 print(rabbits_recur(nr))
 print(rabbits_recur_dict(nr))
-
-
